e4fae527bb1a21dae3108f39a74b305547ed22c3
aaratha <101074206+aaratha@users.noreply.github.com>
2023-07-17 15:32:29 -0400

    div glow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className='bg-bg dark:bg-darkbg text-text dark:text-darktext flex flex-col items-center h-screen'>
       <Header/>
-      <div className='flex flex-col items-center justify-center align-middle m-auto border border-slate-300 p-6 rounded-lg border-opacity-40 -translate-y-8'>
+      <div className='flex flex-col items-center justify-center align-middle m-auto border border-slate-300 p-6 rounded-lg border-opacity-40 -translate-y-8 divGlow'>
         <h1 className='text-center text-5xl m-2'>Ad Funder</h1>
         <p className='text-center m-2'>Turn crowd-funded time into donations.</p>
         <button className='btn-14 custom-btn m-5 p-5'>Sign Up</button>
