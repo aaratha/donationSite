2d3d43d8733fdbda19194de9794455824163d750
aaratha <101074206+aaratha@users.noreply.github.com>
2023-07-17 14:55:40 -0400

    fafsasf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div className='bg-bg dark:bg-darkbg text-text dark:text-darktext flex flex-col items-center h-screen'>
       <Header/>
       <div className='flex flex-col items-center justify-center align-middle m-auto'>
-        <h1 className='text-center text-5xl m-2'>Donation Site</h1>
-        <p className='text-center m-2'>Sign up to receive crowd-funded donations.</p>
+        <h1 className='text-center text-5xl m-2'>Ad Funder</h1>
+        <p className='text-center m-2'>Turn crowd-funded time into donations</p>
         <button className='bg-primary p-2 rounded-md w-24 text-text m-2 hover:scale-105 transition-all'>Sign Up</button>
       </div>
     </div>
